35d0e56d5b69e16b10776924b313779b66cdf4cc
Martijn <martijn.koenis@xs4all.nl>
2014-05-06 11:16:10 +0200

    daadwerkelijk connectie met schrijf en lees voorbeelden

--- Practicum1/Form1.cs
+++ Practicum1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Practicum1
 {
@@ -7,6 +8,41 @@ namespace Practicum1
         public Form1()
         {
             InitializeComponent();
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 3000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "select * from highscores order by score desc";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                label1.Text += "Name: " + reader["name"] + "\tScore: " + reader["score"] + '\n';
+
+        }
+
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
